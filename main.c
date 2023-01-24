7d601e88adc8ffb9e3536a0664ad7f3425d13205
Nthan008 <114546358+Nthan008@users.noreply.github.com>
2023-01-24 17:17:44 +0700

    Update main.c

--- main.c
+++ main.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <stdbool.h>
 #include <unistd.h>
+#include "mylib/mylib.h"
 
 void close_0(){
     printf(" ______________\n");
@@ -20,26 +21,9 @@ void close_0(){
     printf("| | |  ||  | | |\n");
     printf("| | |  ||  | | |\n");
     printf("|_|_|__||__|_|_|\n");
+    printf("  Door is closed   \n");
 }
 
-void close_1(){
-    printf(" ______________\n");
-    printf("|  __________  |\n");
-    printf("| |  ______  | |\n");
-    printf("| | |  ||  | | |\n");
-    printf("| | |  ||  | | |\n");
-    printf("|_|_|__||__|_|_|\n");
-    printf("| |          | |\n");
-    printf("| |          | |\n");
-    printf("| |          | |\n");
-    printf("| |          | |\n");
-    printf("| |          | |\n");
-    printf("| |          | |\n");
-    printf("| |          | |\n");
-    printf("| |          | |\n");
-    printf("| |          | |\n");
-    printf("|_|__________|_|\n");
-}
 
 void open_0(){
     printf(" ______________\n");
@@ -58,6 +42,7 @@ void open_0(){
     printf("| | |      | | |\n");
     printf("| | |      | | |\n");
     printf("|_|_|______|_|_|\n");
+    printf("  Door is opened   \n");
 }
 
 void open_1(){
@@ -77,7 +62,9 @@ void open_1(){
     printf("| |          | |\n");
     printf("| |          | |\n");
     printf("|_|__________|_|\n");
+    printf("  Elevator is moving   \n");
 }
+
 
 void timer_1(){
     printf(" ______________ \n");
@@ -95,6 +82,7 @@ void timer_1(){
     printf("| |          | |\n");
     printf("| |          | |\n");
     printf("|_|__________|_|\n");
+    printf("  Elevator is moving   \n");
 }
 
 void timer_2(){
@@ -113,6 +101,7 @@ void timer_2(){
     printf("| |          | |\n");
     printf("| |          | |\n");
     printf("|_|__________|_|\n");
+    printf("  Elevator is moving   \n");
 }
 
 
@@ -132,6 +121,7 @@ void timer_3(){
     printf("| |          | |\n");
     printf("| |          | |\n");
     printf("|_|__________|_|\n");
+    printf("  Elevator is moving   \n");
 }
 
 void timer_4(){
@@ -150,90 +140,54 @@ void timer_4(){
     printf("| | |  ||  | | |\n");
     printf("| | |  ||  | | |\n");
     printf("|_|_|__||__|_|_|\n");
+    printf("  Elevator is moving   \n");
 }
 
 
           
 
 
-int main() {
-  // Declare variables to represent the current state and input
-  int current_state = 0;
-  int current_input = 0;
-  int current_floor = 0;
-  int input;
-  while (true){
-  if (current_state == 0) {
-    if (current_input == 1) {
-      current_state = 1;
-      int i;
-      for (i = 5; i > 0; i--) {
-        if (current_floor == 0) {
-          if (i == 4){
-            timer_3();
-          }
-          else if (i == 3){
-            timer_2();
-          }
-          else if (i == 2){
-            timer_1();
-          }
-          else if (i == 1){
-            open_1();
-          }
-          sleep(1);
+int main(void) {
+    int s0 = 0, s1 = 0, B, F, O = 0;
+    while (true) {
+        printf("\033[2J");
+        printf("\033[1;1H");
+        if (s0 == 0 && s1 == 0) {
+            close_0();
+        } else if ( s0 == 0 && s1 ==  1){
+            int i;
+            for (i = 5; i > 0; i--) {
+                if (i == 4){
+                    timer_3();
+                }
+                else if (i == 3){
+                    timer_2();
+                }
+                else if (i == 2){
+                    timer_1();
+                }
+                else if (i == 1){
+                    open_1();
+                }
+                sleep(1);
+                printf("\033[2J");
+                printf("\033[1;1H"); 
+            }
+        } else if ( s0 == 1 && s1 == 0){
+            open_0();
         }
-        if (current_floor == 1) {
-          if (i == 4){
-            timer_1();
-          }
-          else if (i == 3){
-            timer_2();
-          }
-          else if (i == 2){
-            timer_3();
-          }
-          else if (i == 1){
-            timer_4();
-          }
-          sleep(1);
+        printf("Enter input (0 to open the door // 1 to close the door): ");
+        scanf("%d", &B);
+        printf("Enter input (0 stop moving // 1 to start moving): ");   
+        scanf("%d", &F);
+
+
+        if (B == 0 || B == 1 && F == 0 || F == 1) {
+            states(s0, s1, B, F, &s0, &s1, &O);
+        } else {
+            printf("Please enter a proper input (0 or 1)\n");
         }
-      }
-      current_state = 2;
-      current_input = 2;
-    }else if (current_input == 2) {
-      current_state = 2;
-      open_0();
     }
-  } 
-  else if (current_state == 2) {
-    if (current_input == 0) {
-      current_state = 0;
-      close_0();
-    }
-    
-  } 
-  
-
-
-  // Read the input from the user
-  printf("Current state: %d\n", current_state);
-  printf("Enter input (0 = Move, 1= Door): ");
-  scanf("%d", &input);
-  if (current_input==0){
-    if (input == 0){
-      current_input = 1;
-    }
-    else if (input == 1){
-      current_input = 2;
-    }
-  } else if (current_input == 2){
-    if (input == 1){
-      current_input = 0;
-    }
-  }
-
-
-  }
-  return 0;
+    return 0;
 }
+
